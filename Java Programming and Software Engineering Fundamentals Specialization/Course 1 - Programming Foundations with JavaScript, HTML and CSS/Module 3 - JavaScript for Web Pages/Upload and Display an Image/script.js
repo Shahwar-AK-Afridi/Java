bc12bed0392a50121a4f169ac349c121ac64fe02
Shahwar-AK-Afridi <Shahwarakafridi@gmail.com>
2026-07-26 17:36:29 +0500

    Upload and Display an Image
    Upload and Display an Image

--- Java Programming and Software Engineering Fundamentals Specialization/Course 1 - Programming Foundations with JavaScript, HTML and CSS/Module 3 - JavaScript for Web Pages/Upload and Display an Image/script.js
+++ Java Programming and Software Engineering Fundamentals Specialization/Course 1 - Programming Foundations with JavaScript, HTML and CSS/Module 3 - JavaScript for Web Pages/Upload and Display an Image/script.js
@@ -2,7 +2,7 @@ function Upload(){
 
   var canvas = document.getElementById("canvas");
   var fileInput = document.getElementById("finput");
-  var image = new SimpleImage(fileInput);
+  var image = new SimpleImage(fileInput);  //Imported Javascript library
   image.drawTo(canvas);
   
 }
